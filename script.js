e6b98ac1f78fe378c8f7238c6aba6e32bae8b876
thesneilert <eilert.gaming@outlook.com>
2023-01-17 13:14:48 +0100

    added buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+let 
+
+function valgfrittNavn1() {
+    document.getElementById('mainDiv').innerHTML = '
+        <div style="background-color: red; width: 100%; heigth: 100px;">
+    ';
+    }
+    function valgfrittNavn2(){
+        document.getElementById('mainDiv').innerHTML = '
+            <div style="background-color: blue; width: 100% heigth: 100px;">
+        ';
+    }
