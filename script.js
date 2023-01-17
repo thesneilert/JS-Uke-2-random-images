c1d8ca905589c00c9240ab0922935c0fcb888f92
thesneilert <eilert.gaming@outlook.com>
2023-01-17 15:01:30 +0100

    fix

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-let 
-
-function valgfrittNavn1() {
-    document.getElementById('mainDiv').innerHTML = '
-        <div style="background-color: red; width: 100%; heigth: 100px;">
-    ';
-    }
-    function valgfrittNavn2(){
-        document.getElementById('mainDiv').innerHTML = '
-            <div style="background-color: blue; width: 100% heigth: 100px;">
-        ';
-    }
+function valgfrittNavn(color, width, height) {
+    document.getElementById('mainDiv').innerHTML = `
+        <div style="background-color: ${color}; width: ${width}%; height: ${height}px;"></div>
+    `;
+}
