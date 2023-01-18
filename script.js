a9b2188771c355c57daf9a46e4dc4dd726709435
thesneilert <eilert.gaming@outlook.com>
2023-01-18 10:46:08 +0100

    made random picxtures generate

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let image_array = [
     "./img/6.jpg"
   ];
 
-  function valgfrittNavn() {
-    let random_index = math.floor(math.random() * image_array.length);
+  function valgfrittNavn(valgfrittElement) {
+    let random_index = Math.floor(Math.random() * image_array.length);
 
     let selected_image = image_array[random_index]
 
-    document.getElementById("random").src = `./img/$(selected_image)`;
+    valgfrittElement.innerHTML = `<img src=${selected_image}></img>`;
   }
 
