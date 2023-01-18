4183549576c01e355eb5f8ee08cf370bf24c1d1e
thesneilert <eilert.gaming@outlook.com>
2023-01-18 09:50:08 +0100

    fix

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function valgfrittNavn(color, width, height) {
-    document.getElementById('mainDiv').innerHTML = `
-        <div style="background-color: ${color}; width: ${width}%; height: ${height}px;"></div>
-    `;
-}
+const images = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg"
+  ];
+  
+  const imageContainer = document.getElementById("images");
+
+  function showRandomImage() {
+    // Clear previous images
+    imageContainer.innerHTML = "";
+    let randomIndex = Math.floor(Math.random() * images.length);
+    let image = document.createElement("img");
+    image.src = images[randomIndex];
+    imageContainer.appendChild(image);
+  }
