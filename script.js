f0f43a7a5786e849cf319f1d43356d30493ab385
thesneilert <eilert.gaming@outlook.com>
2023-01-18 10:32:03 +0100

    fix

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const images = [
-    "img/1.jpg",
-    "img/2.jpg",
-    "img/3.jpg",
-    "img/4.jpg",
-    "img/5.jpg",
-    "img/6.jpg"
+let image_array = [
+    "./img/1.jpg",
+    "./img/2.jpg",
+    "./img/3.jpg",
+    "./img/4.jpg",
+    "./img/5.jpg",
+    "./img/6.jpg"
   ];
-  
-  const imageContainer = document.getElementById("images");
 
-  function showRandomImage() {
-    // Clear previous images
-    imageContainer.innerHTML = "";
-    let randomIndex = Math.floor(Math.random() * images.length);
-    let image = document.createElement("img");
-    image.src = images[randomIndex];
-    imageContainer.appendChild(image);
+  function valgfrittNavn() {
+    let random_index = math.floor(math.random() * image_array.length);
+
+    let selected_image = image_array[random_index]
+
+    document.getElementById("random").src = `./img/$(selected_image)`;
   }
+
