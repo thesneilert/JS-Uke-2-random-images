73df609568ca2e34050137ba64a38a6e54f987f1
thesneilert <eilert.gaming@outlook.com>
2023-01-20 10:34:31 +0100

    added no same image in a row

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+let lastRandomNumber;
+
 function valgfrittIfElse(){
-    let mynumber = Math.ceil(Math.random()* 3);
-    console.log(mynumber)
-    if(mynumber === 1){
+// Genererer tilfeldig nummer
+    let mynumber = Math.ceil(Math.random()* 5);
+// Forsetter å generere nytt nummer til det er anderledes en forrige nummer
+    while (mynumber === lastRandomNumber) {
+        mynumber = Math.ceil(Math.random()* 5);
+    }
+
+// Beholder generert nummer til neste gang
+    lastRandomNumber = mynumber;
+        if(mynumber === 1){
         return './img/1.jpg';
     } 
-    else if(mynumber === 2) {
+        else if(mynumber === 2) {
         return './img/2.jpg';
     }
-    else if(mynumber === 3) {
-       return './img/3.jpg';
+        else if(mynumber === 3) {
+        return './img/3.jpg';
     }
-    else if(mynumber === 4) {
-      return './img/4.jpg';
+        else if(mynumber === 4) {
+        return './img/4.jpg';
     }
-    else {
-      return './img/5.jpg';
+        else {
+        return './img/5.jpg';
     }
 }
 
-function valgfrittNavn(elem) /* sender inn html element*/ {
-    console.log(elem)
+// sender inn bildet til html img element
+function valgfrittNavn(elem) {
     elem.src = valgfrittIfElse();
 }
+
+
+// console.log(mynumber)
+// console.log(elem)
